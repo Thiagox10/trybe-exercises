9739f066abcdb0ae54ff66c8f433a5c20428c4c3
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-06-25 19:01:45 -0300

    Exerciciso 1 e 2

---
+++ exercises/bloco_4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,10 @@
+let n = 5;
+let asterisco = '*';
+let linha = '';
+
+for (let cont = 0; cont < n; cont += 1) {
+    linha = linha + asterisco;
+  };
+  for (let cont = 0; cont < n; cont += 1) {
+    console.log(linha);
+  };
---
+++ exercises/bloco_4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,11 @@
+
+for (cont = 0; cont < 5; cont+=1){
+   let  linha = '';
+   for (cont = 0; cont < 5; cont+=1){
+
+    linha += '*';
+    console.log(linha);
+}
+    }
+    
+    
