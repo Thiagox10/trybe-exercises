0424447335ddf739ee5de89a9e1e73a5c1dc5ce8
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-07-15 19:16:40 -0300

    formulario

---
+++ exercises/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,7 @@
+
+for (let cont = 0; cont < 27; cont+=1){
+    let lista = document.getElementById('estados');
+    let options = document.createElement('option')
+    lista.appendChild(options);
+    // options.setAttribute(required);
+}
