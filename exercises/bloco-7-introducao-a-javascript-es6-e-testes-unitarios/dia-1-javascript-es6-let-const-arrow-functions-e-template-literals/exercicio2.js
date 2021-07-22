0c779cfa32fa5eff395275b57492b5e75b65e49b
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-07-22 20:04:05 -0300

    Parte 1 feita

---
+++ exercises/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio2.js
@@ -0,0 +1,5 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// const compare = (a,b) => a - b;
+
+console.log(`Os numeros ${oddsAndEvens.sort((a,b) => a - b)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
