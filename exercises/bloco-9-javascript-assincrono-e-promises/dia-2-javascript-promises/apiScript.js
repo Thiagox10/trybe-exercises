8d2e7c8b9cc5ae0e3d1c591d39903e346a37f0c2
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-08-11 19:15:22 -0300

    Exercicio API

---
+++ exercises/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/apiScript.js
@@ -0,0 +1,42 @@
+// apiScript.js     
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+const promise = new Promise((resolve, reject) => {
+  function getRandom(min, max) {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.pow(result, 2);
+}
+
+let array = [];
+let soma = 0;
+for (let i = 0 ; i < 10 ; i++){
+  array.push(getRandom(1,50));
+    soma += array[i];
+}
+
+console.log(array);
+console.log(soma);
+
+})
+.then(() => {
+  const didOperationSucceed = soma < 8000;
+if(didOperationSucceed) {
+  
+ return console.log('Promise resolvida')
+}
+
+})
+.catch(console.log('Promise rejeitada'))
+
+window.onload = () => fetchJoke();
